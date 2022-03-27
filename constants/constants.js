8270016d408c630621aa8dfcf94d85d869f9fb68
Jeremy Roy <46678222+Mellowlink@users.noreply.github.com>
2022-03-27 22:48:19 +0900

    Fix paths, add social media, add homepage splash image, styling etc.

--- constants/constants.js
+++ constants/constants.js
@@ -3,27 +3,71 @@ const strings = {
 
 }
 
+const images = {
+  homeImageDesktop: "title_v02.png",
+  homeImageMobile: "title_v02.png",
+}
+
 const menuItems = [
   {
     text: "Storyboards",
-    path: "storyboards",
+    path: "/storyboards",
   },
   {
     text: "Design",
-    path: "design",
+    path: "/design",
   },
   {
     text: "Sketchbook",
-    path: "sketchbook",
+    path: "/sketchbook",
   },
   {
     text: "Fine Art",
-    path: "fineart",
+    path: "/fineart",
   },
   {
     text: "Contact",
-    path: "contact",
+    path: "/contact",
   },
 ]
 
-const images = {}
+/*
+CONFIGURABLE, accepted types:
+Instagram: instagram
+LinkedIn: linkedin, linkedin-in
+Facebook: facebook, facebook-f, facebook-square
+Twitter: twitter, twitter-square
+Google Plus: google-plus, google-plus-g, google-plus-square
+Vimeo: vimeo, vimeo-v, vimeo-square
+Youtube: youtube
+*/
+const socialMedia = [
+  {
+    type: 'instagram',
+    url: 'https://www.instagram.com/beckbrook.art/',
+  },
+  {
+    type: 'linkedin',
+    url: 'https://www.linkedin.com/in/rebecca-brook-art',
+  },
+  // {
+  //   type: 'facebook',
+  //   url: 'example',
+  // },
+  // {
+  //   type: 'twitter',
+  //   url: 'example',
+  // },
+  // {
+  //   type: 'google-plus',
+  //   url: 'example',
+  // },
+  // {
+  //   type: 'vimeo',
+  //   url: 'example',
+  // },
+  // {
+  //   type: 'youtube',
+  //   url: 'example',
+  // }
+]
--- js/script.js
+++ js/script.js
@@ -2,22 +2,47 @@ const updatePageContent = (strings, page) => {
   console.log('Hey Sneed');
 }
 
+const updateImages = (images, page) => {
+  switch(page){
+    case 'home':
+      $('#home-image-desktop').attr("src","./assets/images/"+images.homeImageDesktop);
+      $('#home-image-desktop').attr("alt",images.homeImageDesktop);
+      $('#home-image-mobile').attr("src","./assets/images/"+images.homeImageMobile);
+      $('#home-image-mobile').attr("alt",images.homeImageMobile);
+      break;
+  }
+}
+
 const updateMenuItems = (menuItems, page) => {
-  var rootUrl = window.location.origin;
+  var rootUrl = '.';
   var pathUrl = window.location.pathname;
+
+  if (pathUrl != '/'){
+    rootUrl = '..'
+  }
   //loop through the configured menu items and populate the navbar
   var items = '';
   menuItems.map((item) => {
     var disabledClass = '';
-    if (pathUrl == '/'+item.path){
+    if (pathUrl == item.path){
       disabledClass = ' disabled';
     }
     items = items.concat('<li class="nav-item"><a class="nav-link'+disabledClass+'" href="'
-    + rootUrl + '/' + item.path + '">' + item.text + '</a></li>');
+    + rootUrl + item.path + '">' + item.text + '</a></li>');
   });
   document.getElementById("nav-items").innerHTML = items;
   document.getElementById("nav-items-top").innerHTML = items;
   document.getElementById("nav-items-mobile").innerHTML = items;
+}
+
+const updateSocials = (socialMedia) => {
+  //loop through the configured social media items and populate the icon links
+  var items = '';
+  socialMedia.map((item) => {
+    items = items.concat('<a href="'+item.url+'" class="me-4"><i class="fab fa-'+item.type+' fa-lg"></i></a>');
+  });
+  document.getElementById("mobile-socials").innerHTML = items;
+  document.getElementById("footer-socials").innerHTML = items;
 }
 
 const showMenu = () => {
@@ -32,3 +57,4 @@ jQuery($ => {
     $('.navbar-drop-out').toggleClass('navbar-dropped', scroll >= 250);
   });
 });
+//©
