255e604037f6e034c5eca63b2e9445bb3c69c4ce
Jeremy Roy <46678222+Mellowlink@users.noreply.github.com>
2022-03-31 18:14:48 +0900

    Fix font loading, rename constants to config, comment config file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,40 @@
+// CONTENT
+
+/*
+CONFIGURABLE
+Text Content
+
+logoText: The name to be displayed on the header, also used as a link back to the homepage
+bioText: displayed on the contact page
+emailAddress: internet tube street address
+*/
 const strings = {
   logoText: "Rebecca Brook", //Amatic
   bioText: "Rebecca Brook is an artist and animator who graduated with distinction from the BFA Drawing & Painting program at OCADU in 2014. In 2018, she earned an advanced diploma from Loyalist College in Animation and Game Development.<br><br>Rebecca has worked many roles at Taqqut Productions since 2018 including designer and storyboard artist. Currently, she is the pre-production supervisor for the studio’s in-house animation projects.",
   emailAddress: "rebecca.c.brook@gmail.com",
 }
 
+/*
+CONFIGURABLE
+Filenames of images to display, no path required.
+
+homeImageDesktop: The homepage splash image to display in the desktop layout
+homeImageMobile: The homepage splash image to display in the mobile layout
+contactImage: The contact page image to display alongside the bio
+
+-* images must be placed in the "/assets/images/design" folder
+designImages: An array of filenames of images to display on the design page gallery (seperated by comma)
+
+-* images must be placed in the "/assets/images/sketchbook" folder
+sketchbookImages: An array of filenames of images to display on the sketchbook page gallery (seperated by comma)
+
+-* images must be placed in the "/assets/images/fineart" folder
+fineArtImages: An array of objects {} for images & descriptions to display on the fine art page gallery (seperated by comma)
+- Required Attributes:
+- - filename: the filename of the image to display
+- - title: the title to be displayed on the fullsize image
+- - description: the description to be displayed on the fullsize image, can contain line breaks <br>
+*/
 const images = {
   homeImageDesktop: "title_v02.png",
   homeImageMobile: "title_v02-placeholder-flipped.png",
@@ -94,6 +125,15 @@ const images = {
   ],
 }
 
+/*
+CONFIGURABLE: an array of objects {} to build the video sections
+
+Required attributes:
+url: The embed link, supports Vimeo & Youtube, any others are untested
+title: text
+subtitle: text
+description: text, can contain html line breaks, just put a <br> where you want the line to break
+*/
 const videos = [
   {
     url: 'https://player.vimeo.com/video/253989945?h=c6db007fe5&color=ef0800&title=0&byline=0&portrait=0',
@@ -109,9 +149,22 @@ const videos = [
   },
 ]
 
+/*
+CONFIGURABLE: an array of objects {} to build the slideshow sections, seperated by commas
+
+-* images must be placed in the "/assets/images/storyboards" folder
+
+Required attributes:
+key: used to properly build the page elements this should be unique, not spaces or special characters other than underscore and hyphen
+title: text
+subtitle: text
+description: text, can contain html line breaks, just put a <br> where you want the line to break
+images: an array of filenames including extension (png, jpg, gif, etc.), seperated by commas.
+        Min: 1, Max: 14-15(ideal), but technically infinite
+*/
 const slideshows = [
   {
-    key: 'title', //this should be unique, not spaces or special characters other than underscore and hyphen
+    key: 'title',
     title: 'Title',
     subtitle: 'A Slideshow (Winter 1349)',
     description: 'This is a slideshow from some year or whatever lol',
@@ -125,7 +178,7 @@ const slideshows = [
     ]
   },
   {
-    key: 'testy-test', //this should be unique, not spaces or special characters other than underscore and hyphen
+    key: 'testy-test',
     title: 'Testy test',
     subtitle: 'A test slidey show (Autumn 2007)',
     description: 'This sure is a test, plus more text to make it longer for design placeholder purposes etc.',
@@ -136,29 +189,6 @@ const slideshows = [
       'hiest4.png',
       'hiest5.png',
     ]
-  },
-]
-
-const menuItems = [
-  {
-    text: "Storyboards",
-    path: "/storyboards",
-  },
-  {
-    text: "Design",
-    path: "/design",
-  },
-  {
-    text: "Sketchbook",
-    path: "/sketchbook",
-  },
-  {
-    text: "Fine Art",
-    path: "/fineart",
-  },
-  {
-    text: "Contact",
-    path: "/contact",
   },
 ]
 
@@ -202,3 +232,32 @@ const socialMedia = [
   //   url: 'example',
   // },
 ]
+// END CONTENT
+
+// NAVIGATION
+/*
+Used to populated the top and mobile menus
+*/
+const menuItems = [
+  {
+    text: "Storyboards",
+    path: "/storyboards",
+  },
+  {
+    text: "Design",
+    path: "/design",
+  },
+  {
+    text: "Sketchbook",
+    path: "/sketchbook",
+  },
+  {
+    text: "Fine Art",
+    path: "/fineart",
+  },
+  {
+    text: "Contact",
+    path: "/contact",
+  },
+]
+// END NAVIGATION
